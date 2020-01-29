1ded92e05ab233213e1b40f7c12b266c9a68c204
PAULO <pasfraga@gmail.com>
2020-01-28 21:38:39 -0300

    at02

--- footer.php
+++ footer.php
@@ -10,16 +10,28 @@
   <div class="container">
             <div class="row">
                 <div class="col-sm-3">
-                    <h5>Categorias</h5>
-
+                    <h5 class="fas fa-filter"> Categoria<h5>
+                    <div>
+                    <h6 class="fas fa-tools"><a href="facebook.com.br/HomeShop"> Ferramentas</a><h6>
+                    <h6 class="fas fa-bolt"><a href="facebook.com.br/HomeShop"> Eletronicos</a><h6>
+                    <h6 class="	fas fa-sim-card"><a href="facebook.com.br/HomeShop"> Celulares</a><h6>
+                    <h6 class="fas fa-bed"><a href="facebook.com.br/HomeShop">Cama,Mesa e Banho</a><h6>
+                    </div>
                 </div>
                 <div class="col-sm-3">
-					<h5>Mapa do site</h5>
+					<h5 class="far fa-hand-point-right"> Mapa do site</h5>
+                    <div>
+                    <h6 class="fas fa-align-justify"><a href="facebook.com.br/HomeShop"> Home</a><h6>
+                    <h6 class="fas fa-box-open"><a href="facebook.com.br/HomeShop"> Produtos</a><h6>
+                    <h6 class="fas fa-comment-dollar"><a href="facebook.com.br/HomeShop"> Comprar</a><h6>
+                    <h6 class="far fa-comment-dots"><a href="facebook.com.br/HomeShop">Contato</a><h6>
+                    <h6 class="far fa-grin-beam"><a href="facebook.com.br/HomeShop">Cadastro</a><h6>
+                    </div>
                    	               
                 </div>
                 <div class="col-sm-3">
                     <h5>Redes Sociais</h5>
-					<h4><i class="fas fa-laptop" aria-hidden="true"><a href="facebook.com.br"></i></h4>
+					<h4><i class="fab fa-facebook"><a href="facebook.com.br/HomeShop">Facebook</i></h4>                    
 					
                 </div>
             </div>
